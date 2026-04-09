939eb286273aed5c5c49168f062660c8825443ae
carloscoss <crobertohenriquez@gmail.com>
2026-04-09 15:15:30 -0500

    Project done

--- src/main/java/com/pluralsight/Calculator.java
+++ src/main/java/com/pluralsight/Calculator.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+
+        // Adding the first number
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter the first number: ");
+        int number1 = reader.nextInt();
 
-        String number1 = reader.nextLine();
+        // adding the second number
         System.out.println("Enter the second number: ");
-        String number2 = reader.nextLine();
+        int number2 = reader.nextInt();
+
+        //making the operation
+        int result = number1 * number2;
+        System.out.println("The multiplication of these numbers is: " + result);
+
     }
 }
